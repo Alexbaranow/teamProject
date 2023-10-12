93ce95841d07881201faf879caac46406875f1e1
Vallydei <obedkova.v@rambler.ru>
2023-10-12 20:06:52 +0300

    auth and reg rare

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Authorization from './ui/Authorization';
 import Registration from './ui/Regisration';
 import useUser from '../Hooks/UseUser';
 
-export default function App({ user }) {
-  const { signInHandler, signUpHandler } = useUser(user);
+export default function App() {
+  const { signInHandler, signUpHandler } = useUser();
+  const [comments, setComments] = useState([{id:1}, {id:2}, {id:3}])
+  const deleteHandler = () => {
+    setComments([]) 
+  }
   return (
     <div className="container">
       <Routes>
         <Route path="/" element={<Authorization signInHandler={signInHandler} />} />
-        <Route path="/auth/signup" element={<Registration signUpHandler={signUpHandler} />} />
+        <Route path="/api/auth/signup" element={<Registration signUpHandler={signUpHandler} />} />
       </Routes>
     </div>
   );
--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -1,10 +1,37 @@
 import express from 'express';
+import bcrypt from 'bcrypt';
+import { User } from '../../db/models';
 
-const router = express.Router();
+const indexRouter = express.Router();
 
-router.get('/', (req, res) => {
-  const initState = { hello: 'world' };
+indexRouter.post('/', async (req, res) => {
+  const { email, pass } = req.body;
+  if (!email || !pass) {
+    res.status(400).json({ message: 'все поля обязательны к заполнению' });
+    return;
+  }
+
+  const user = await User.findOne({
+    where: {
+      email,
+    },
+  });
+
+  if (!user || !await bcrypt.compare(pass, user.pass)) {
+    res.status(400).json({ message: 'неверный логин или пароль' });
+    return;
+  }
+  req.session.user = {
+    name: user.name,
+    email: user.email,
+    id: user.id,
+  };
+  res.sendStatus(200);
+});
+
+indexRouter.get('/', (req, res) => {
+  const initState = {};
   res.render('Layout', initState);
 });
 
-export default router;
+export default indexRouter;
