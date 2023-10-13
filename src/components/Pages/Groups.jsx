c01dea3b58ac30e772d320ce52aece23b205eeef
Nikotine <nikita12360732@gmail.com>
2023-10-13 16:35:10 +0300

    release final

--- src/components/Pages/Groups.jsx
+++ src/components/Pages/Groups.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Button, Container, Form } from 'react-bootstrap';
-import { ReCAPTCHA } from 'react-google-recaptcha';
+// import { ReCAPTCHA } from 'react-google-recaptcha';
+import { lazy } from 'react';
 
 export default function Groups({ user, groups, groupSubmitHandler }) {
+  const GoogleCaptcha = lazy(() => import('react-google-recaptcha'));
   const onChange = () => {};
   return (
     <Container>
@@ -21,7 +23,12 @@ export default function Groups({ user, groups, groupSubmitHandler }) {
         <br />
         <Button variant="dark" as="input" type="submit" value="Submit" />
       </Form>
-      <ReCAPTCHA sitekey="6LcL5pooAAAAAJy5l0fnxAyEjxLjmhCkcE5cA-Jn" onChange={onChange} />
+      <h1>111</h1>
+      <Suspense fallback={<h1>...</h1>}>
+        <h2>Preview</h2>
+        <GoogleCaptcha sitekey="6LcL5pooAAAAAJy5l0fnxAyEjxLjmhCkcE5cA-Jn" onChange={onChange} />
+      </Suspense>
+      {/* <ReCAPTCHA sitekey="6LcL5pooAAAAAJy5l0fnxAyEjxLjmhCkcE5cA-Jn" onChange={onChange} /> */}
     </Container>
   );
 }
